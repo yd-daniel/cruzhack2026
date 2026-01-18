b0d0d1e12e66a36563b901fb7e48bacc7f60d938
Ian Keilman <iankeilman@proton.me>
2026-01-18 02:21:59 -0800

    Update Oled.cpp
    Finished drawBarPencentage, made compatible with main

--- lib/Oled/Oled.cpp
+++ lib/Oled/Oled.cpp
@@ -4,8 +4,11 @@
  * Defines oled data struct and oled menu options and functions
  */
 
-#include "Oled.h"
+#include "oled.h"
 #include <string.h>
+
+void drawBar(char *xbm, int barIndex, int barHeight);
+void drawBarPercentage(char *xbm, int barPercent);
 
 /**
  * Initialize the OLED UI state
@@ -33,10 +36,17 @@ void Oled_getBarChartXBM(char *xbm, float *decibelArray)
   memcpy(xbm, BAR_CHART_XBM, sizeof(BAR_CHART_XBM));
 
   // Draw Bars
-  for (int i = 0; i < 28; i++)
+  for (int i = 0; i < BAR_CHART_SIZE; i++)
   {
     decibelPercent = decibelArray[i] / MAX_DECIBEL;
-    drawBar(xbm, i, decibelPercent * BAR_CHART_MAX_HEIGHT);
+    if (decibelPercent < 0.0f) decibelPercent = 0.0f;
+    if (decibelPercent > 1.0f) decibelPercent = 1.0f;
+
+    int barHeight = (int)(decibelPercent * BAR_CHART_MAX_HEIGHT);
+    if (barHeight < 0) barHeight = 0;
+    if (barHeight > BAR_CHART_MAX_HEIGHT) barHeight = BAR_CHART_MAX_HEIGHT;
+
+    drawBar(xbm, i, barHeight);
   }
 }
 
@@ -77,7 +87,12 @@ void Oled_getDecibelXBM(char *xbm)
  */
 void Oled_getWarningXBM(char *xbm, float percentFilled)
 {
+  int barPercent = (int)(percentFilled * 100.0f);
+  if (barPercent < 0) barPercent = 0;
+  if (barPercent > 100) barPercent = 100;
+
   memcpy(xbm, WARNING_XBM, sizeof(WARNING_XBM));
+  drawBarPercentage(xbm, barPercent);
 }
 
 /**
@@ -115,8 +130,20 @@ void drawBarPercentage(char *xbm, int barPercent)
   const int BAR_ZERO = (42 * DISPLAY_WIDTH) + 1; // bytes, bottom left of progress bar
   const int BAR_ZERO_PIXEL = 3;                  // pixels
 
+  int fillPixels = (BAR_WIDTH * barPercent) / 100;
+  if (fillPixels < 0) fillPixels = 0;
+  if (fillPixels > BAR_WIDTH) fillPixels = BAR_WIDTH;
+
   for (int i = 0; i < BAR_HEIGHT; i++)
   {
-    
+    int rowStart = BAR_ZERO + (i * (-DISPLAY_WIDTH));
+
+    for (int x = 0; x < fillPixels; x++)
+    {
+      int bitPos = BAR_ZERO_PIXEL + x;
+      int byteOffset = bitPos / 8;
+      int bitOffset = bitPos % 8;
+      xbm[rowStart + byteOffset] |= (1 << bitOffset);
+    }
   }
 }
